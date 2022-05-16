d22195ab724e73d3f25f4c6064968bad1a9e6d97
iamvcm848 <valenmascarenhas14@gmail.com>
2022-05-16 22:39:06 +0530

    files added

---
+++ helmet_detection.py
@@ -0,0 +1,60 @@
+import cv2
+import numpy as np
+import glob
+import random
+
+net=cv2.dnn.readNet('yolov3-helmet.weights','yolov3-helmet.cfg')
+
+classes = []
+
+with open('helmet.names','r') as f:
+    classes = f.read().splitlines()
+
+img= cv2.imread('images/p.jpg')
+height,width,_=img.shape
+
+blob=cv2.dnn.blobFromImage(img,1/255,(416,416),(0,0,0),swapRB=True,crop=False)
+net.setInput(blob)
+output_layers_names=net.getUnconnectedOutLayersNames()
+layerOutputs=net.forward(output_layers_names)
+
+boxes=[]
+confidences=[]
+class_ids=[]
+
+for output in layerOutputs:
+    for detection in output:
+        scores= detection[5:]
+        class_id=np.argmax(scores)
+        confidence=scores[class_id]
+        
+        if confidence>0.5:
+            center_x=int(detection[0]*width)
+            center_y=int(detection[1]*height)
+            w=int(detection[2]*width)
+            h=int(detection[3]*height)
+            x = int(center_x - w / 2)
+            y = int(center_y - h / 2)
+            confidences.append(float(confidence))
+            boxes.append([x, y, w, h])
+            class_ids.append(class_id)
+
+indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+print(indexes)
+
+font=cv2.FONT_HERSHEY_PLAIN
+colors=np.random.uniform(0,255,size=(len(boxes),3))
+
+if len(indexes)>0:
+    for i in indexes.flatten():
+        x, y, w,h = boxes[i]
+        label = str(classes[class_ids[i]])
+        confidence = str(round(confidences[i], 2))
+        color = colors[i]
+        cv2.rectangle(img, (x, y), (x+w, y+h), color, 2)
+        cv2.putText(img, label + " " + confidence,
+                (x, y+20), font, 2, (255, 255, 255), 2)
+
+cv2.imshow('Project',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
